8fccce8aafcf093e18df66cc129e0f699012a07b
LLANNILLO <minillanillo@gmail.com>
2025-12-11 16:31:02 +0100

    First Commit
    Creating our first GoLang server.
    Added the logic to start the server at / with http.ServerMux,
    http.Server{} & FilePath, FileServer & http.Dir()

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+
+	mux.Handle("/", fileServer)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
